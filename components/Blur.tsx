0b6a0f70f9fa1c0dc653547f4b65d216b4076bc6
figueroaignacio <ignaciofigueroadev@gmail.com>
2024-04-14 22:39:48 -0300

    style: removing blur component in the mobile view

--- components/Blur.tsx
+++ components/Blur.tsx
@@ -3,6 +3,6 @@ import "../styles/animations.css";
 
 export function Blur() {
   return (
-    <div className="fixed top-0 bottom-0 z-[-2] min-h-screen w-full dark:bg-radial-gradient-desktop"></div>
+    <div className="hidden md:flex md:fixed top-0 bottom-0 z-[-2] min-h-screen w-full dark:bg-radial-gradient-desktop"></div>
   );
 }
