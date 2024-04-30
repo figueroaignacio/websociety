797e25a3587a9d1cd58bf6806660845012ec4607
figueroaignacio <ignaciofigueroadev@gmail.com>
2024-04-30 16:15:20 -0300

    feat(components): changing the logo

--- components/trademark.tsx
+++ components/trademark.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export function Logo() {
   return (
     <Link href="/">
-      <h2 className="font-bold">iCoded</h2>
+      <h2 className="font-bold">fs()</h2>
     </Link>
   );
 }
