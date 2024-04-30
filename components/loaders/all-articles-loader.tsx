407578b4ad31d3aaf1669ba53526fc463eeaa124
figueroaignacio <ignaciofigueroadev@gmail.com>
2024-04-30 16:37:39 -0300

    fix(loaders): fixing the loaders

--- components/loaders/all-articles-loader.tsx
+++ components/loaders/all-articles-loader.tsx
@@ -5,11 +5,9 @@ import { ArticleCardLoader } from "@/components/loaders/article-card-loader";
 export function AllArticlesLoader() {
   return (
     <div className="flex flex-col gap-2 py-10">
-      <div className="flex flex-col gap-3 justify-center py-52 lg:py-56">
-        <h3 className="font-bold text-5xl lg:text-8xl inline-block bg-gradient-to-r text-transparent bg-clip-text dark:from-gray-400 dark:via-gray-700 dark:to-gray-950 from-gray-900 via-gray-600 to-gray-300">
-          Articles
-        </h3>
-        <p className="text-xs lg:text-sm opacity-75">
+      <div className="flex flex-col gap-3 justify-center py-52 lg:py-56 text-center">
+        <h3 className="font-bold text-5xl lg:text-7xl">Articles</h3>
+        <p className="text-xs lg:text-lg opacity-75">
           In this section you are going to find articles about tech and coding.
           Check them!
         </p>
--- components/loaders/all-posts-loader.tsx
+++ components/loaders/all-posts-loader.tsx
@@ -5,11 +5,9 @@ export function AllPostsLoader() {
   return (
     <section className="py-10">
       <div className=" flex flex-col gap-5">
-        <div className="flex flex-col gap-3 justify-center py-52 lg:py-56">
-          <h3 className="font-bold text-5xl lg:text-8xl inline-block bg-gradient-to-r text-transparent bg-clip-text dark:from-gray-400 dark:via-gray-700 dark:to-gray-950 from-gray-900 via-gray-600 to-gray-300">
-            Posts
-          </h3>
-          <p className="text-xs lg:text-sm opacity-75">
+        <div className="flex flex-col gap-3 justify-center py-52 lg:py-56 text-center">
+          <h3 className="font-bold text-5xl lg:text-7xl">Posts</h3>
+          <p className="text-xs lg:text-lg opacity-75">
             In this section you are going to find quick posts as tips and
             advices.
           </p>
