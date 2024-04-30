a05dbfeb5bcd6ce70e760179e8c2ce70b81cffe0
figueroaignacio <ignaciofigueroadev@gmail.com>
2024-04-30 16:54:57 -0300

    fix(loaders): fixing titles and descriptions

--- components/loaders/all-articles-loader.tsx
+++ components/loaders/all-articles-loader.tsx
@@ -2,14 +2,16 @@
 import { ArticleSearch } from "@/components/article-search";
 import { ArticleCardLoader } from "@/components/loaders/article-card-loader";
 
+// Utils
+import { allArticles } from "@/constants/articles";
+
 export function AllArticlesLoader() {
   return (
     <div className="flex flex-col gap-2 py-10">
       <div className="flex flex-col gap-3 justify-center py-52 lg:py-56 text-center">
-        <h3 className="font-bold text-5xl lg:text-7xl">Articles</h3>
+        <h3 className="font-bold text-5xl lg:text-7xl">{allArticles.title}</h3>
         <p className="text-xs lg:text-lg opacity-75">
-          In this section you are going to find articles about tech and coding.
-          Check them!
+          {allArticles.description}
         </p>
       </div>
       <div className="flex">
--- components/loaders/all-posts-loader.tsx
+++ components/loaders/all-posts-loader.tsx
@@ -1,15 +1,17 @@
 // Components
 import { PostCardLoader } from "@/components/loaders/post-card-loader";
 
+// Utils
+import { allPosts } from "@/constants/posts";
+
 export function AllPostsLoader() {
   return (
     <section className="py-10">
       <div className=" flex flex-col gap-5">
         <div className="flex flex-col gap-3 justify-center py-52 lg:py-56 text-center">
-          <h3 className="font-bold text-5xl lg:text-7xl">Posts</h3>
+          <h3 className="font-bold text-5xl lg:text-7xl">{allPosts.title}</h3>
           <p className="text-xs lg:text-lg opacity-75">
-            In this section you are going to find quick posts as tips and
-            advices.
+            {allPosts.description}
           </p>
         </div>
         <div className="grid gap-5 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-center items-center pt-10">
