81649dd91e5770b53ca42f9d98b9a20cbe123306
figueroaignacio <ignaciofigueroadev@gmail.com>
2024-05-05 00:47:30 -0300

    chore(fonts): removing ubuntu font file

--- font/ubuntu.ts
+++
@@ -1,8 +0,0 @@
-import { Ubuntu } from "next/font/google";
-
-export const ubuntu = Ubuntu({
-  weight: ["300", "400", "500", "700"],
-  variable: "--ubuntu-font",
-  display: "auto",
-  subsets: ["latin"],
-});
