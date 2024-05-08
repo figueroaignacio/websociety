5b97098445a7ecd0599bf5186507feac361b6f73
figueroaignacio <ignaciofigueroadev@gmail.com>
2024-05-08 17:59:05 -0300

    chore(config): removing github path

--- config/navigation.ts
+++ config/navigation.ts
@@ -14,11 +14,6 @@ export const navItems = Object.freeze([
     href: "/articles",
     icon: "article",
   },
-  {
-    title: "Github",
-    href: "https://github.com/figueroaignacio/frontsociety/",
-    icon: "github",
-  },
 ]);
 
 export const exploreItems = Object.freeze([
