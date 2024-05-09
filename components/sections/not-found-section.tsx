b24027ced7a3f3735f2cd41d7617c252215aad1c
figueroaignacio <ignaciofigueroadev@gmail.com>
2024-05-09 17:55:50 -0300

    chore(components): adding theme provider to the not found component

--- components/sections/not-found-section.tsx
+++ components/sections/not-found-section.tsx
@@ -1,3 +1,6 @@
+// Provider
+import { ThemeProvider } from "@/providers/theme-provider/theme-provider";
+
 // Components
 import { BackButton } from "@/components/BackButton";
 
@@ -9,17 +12,24 @@ import { notFound } from "@/constants/errors";
 
 export function NotFoundSection() {
   return (
-    <section className="min-h-[90vh] flex justify-center items-center">
-      <div className="flex flex-col justify-center items-center gap-3 text-center border-[.0625rem] rounded-md p-16 border-dashed">
-        <div className="border-[1px] rounded-full p-3 border-dashed">
-          <HelpCircle size={"1.5rem"} />
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+    >
+      <section className="min-h-[90vh] flex justify-center items-center">
+        <div className="flex flex-col justify-center items-center gap-3 text-center border-[.0625rem] rounded-md p-16 border-dashed">
+          <div className="border-[1px] rounded-full p-3 border-dashed">
+            <HelpCircle size={"1.5rem"} />
+          </div>
+          <div>{notFound.notFoundErrorCode}</div>
+          <div>
+            <p className="opacity-75">{notFound.notFoundDescription}</p>
+          </div>
+          <BackButton title="Go Back" />
         </div>
-        <div>{notFound.notFoundErrorCode}</div>
-        <div>
-          <p className="opacity-75">{notFound.notFoundDescription}</p>
-        </div>
-        <BackButton title="Go Back" />
-      </div>
-    </section>
+      </section>
+    </ThemeProvider>
   );
 }
