1843d34a853c75c91658b20b45ff39d214bc515b
figueroaignacio <ignaciofigueroadev@gmail.com>
2024-05-09 17:46:49 -0300

    feat(components): adding not found sectipn

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,29 +1,8 @@
 // Components
-import { BackButton } from "@/components/BackButton";
+import { NotFoundSection } from "@/components/sections/not-found-section";
 
 // Utils
-import { notFound } from "@/constants/errors";
-
-import { ThemeProvider } from "@/providers/theme-provider/theme-provider";
 
 export default async function NotFound() {
-  return (
-    <ThemeProvider
-      attribute="class"
-      defaultTheme="system"
-      enableSystem
-      disableTransitionOnChange
-    >
-      <section className="flex flex-col justify-center items-start gap-5 min-h-screen m-auto container max-w-6xl">
-        <h2 className="font-bold text-5xl lg:text-8xl inline-block bg-gradient-to-r text-transparent bg-clip-text dark:from-gray-400 dark:via-gray-700 dark:to-gray-950 from-gray-900 via-gray-600 to-gray-300">
-          {notFound.notFoundErrorCode}
-        </h2>
-        <p className="text-2xl">{notFound.notFoundTitle}</p>
-        <p className="text-sm opacity-75">{notFound.notFoundDescription}</p>
-        <div className="flex items-center justify-center gap-4">
-          <BackButton title="Back Home" />
-        </div>
-      </section>
-    </ThemeProvider>
-  );
+  return <NotFoundSection />;
 }
---
+++ components/sections/not-found-section.tsx
@@ -0,0 +1,25 @@
+// Components
+import { BackButton } from "@/components/BackButton";
+
+// Icons
+import { HelpCircle } from "lucide-react";
+
+// Constants
+import { notFound } from "@/constants/errors";
+
+export function NotFoundSection() {
+  return (
+    <section className="min-h-[90vh] flex justify-center items-center">
+      <div className="flex flex-col justify-center items-center gap-3 text-center border-[.0625rem] rounded-md p-16 border-dashed">
+        <div className="border-[1px] rounded-full p-3 border-dashed">
+          <HelpCircle size={"1.5rem"} />
+        </div>
+        <div>{notFound.notFoundErrorCode}</div>
+        <div>
+          <p className="opacity-75">{notFound.notFoundDescription}</p>
+        </div>
+        <BackButton title="Go Back" />
+      </div>
+    </section>
+  );
+}
