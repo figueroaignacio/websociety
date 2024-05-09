862550afa45e60d535eff1904aad818cbb894724
figueroaignacio <ignaciofigueroadev@gmail.com>
2024-05-09 17:53:24 -0300

    chore(constants): changing some messages

--- constants/errors.ts
+++ constants/errors.ts
@@ -5,9 +5,7 @@ export const errors = Object.freeze({
 
 export const notFound = Object.freeze({
   notFoundErrorCode: "404",
-  notFoundTitle: "Oops! This page could not be found.",
-  notFoundDescription:
-    "Sorry but the page you are looking for does not exist, was not created, have been removed, name changed or is temporarily unavailable",
+  notFoundDescription: "The page you are looking for does not exist.",
 });
 
 export const inDevelopmentSection = Object.freeze({
