c0bfcbf18ccf3b2c9c97e25b8bdb0ef128471cb9
figueroaignacio <ignaciofigueroadev@gmail.com>
2024-05-15 17:57:10 -0300

    build: adding velite dependency

---
+++ velite.config.ts
@@ -0,0 +1,33 @@
+import { defineCollection, defineConfig, s } from "velite";
+
+const computedFields = <T extends { slug: string }>(data: T) => ({
+  ...data,
+  slugAsParams: data.slug.split("/").slice(1).join("/"),
+});
+
+const posts = defineCollection({
+  name: "Post",
+  pattern: "posts/**/*.mdx",
+  schema: s
+    .object({
+      slug: s.path(),
+      title: s.string().max(99),
+      description: s.string().max(9999).optional(),
+      date: s.isodate(),
+      published: s.boolean().default(true),
+      body: s.mdx(),
+    })
+    .transform(computedFields),
+});
+
+export default defineConfig({
+  root: "content",
+  output: {
+    data: ".velite",
+    assets: "public/static",
+    base: "/static/",
+    name: "[name]-[hash:6].[ext]",
+    clean: true,
+  },
+  collections: { posts },
+});
