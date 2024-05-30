a74bbcba2f9db0f5264e6d5a61267f995d04bee8
figueroaignacio <ignaciofigueroadev@gmail.com>
2024-05-30 15:48:42 -0300

    feat: setting up tags

--- velite.config.ts
+++ velite.config.ts
@@ -18,9 +18,10 @@ const posts = defineCollection({
     .object({
       slug: s.path(),
       title: s.string().max(99),
-      description: s.string().max(9999).optional(),
+      description: s.string().max(999).optional(),
       date: s.isodate(),
       published: s.boolean().default(true),
+      tags: s.array(s.string()).optional(),
       body: s.mdx(),
     })
     .transform(computedFields),
