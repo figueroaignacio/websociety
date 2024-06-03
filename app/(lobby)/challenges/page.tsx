cbe3aca0182e10ccb011dd38c0fd73f0a0879224
figueroaignacio <ignaciofigueroadev@gmail.com>
2024-06-03 18:59:39 -0300

    chore: removing in development section

--- app/(lobby)/challenges/page.tsx
+++ app/(lobby)/challenges/page.tsx
@@ -1,7 +1,6 @@
-// Section
-import { InDevelopment } from "@/components/in-development";
-
-// Metadata
+import { BackButton } from "@/components/back-button";
+import { inDevelopmentSection } from "@/config/errors";
+import { Rocket } from "lucide-react";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -11,5 +10,17 @@ export const metadata: Metadata = {
 };
 
 export default function ChallengesPage() {
-  return <InDevelopment />;
+  return (
+    <section className="min-h-[90vh] flex justify-center items-center">
+      <div className="flex flex-col justify-center items-center gap-3 text-center border-2 rounded-md p-16 border-dashed">
+        <div className="border-2 rounded-full p-3">
+          <Rocket size="1.5rem" />
+        </div>
+        <div>
+          <p className="opacity-75">{inDevelopmentSection.description}</p>
+        </div>
+        <BackButton title="Go Back" />
+      </div>
+    </section>
+  );
 }
