348bfe1a7dee28ff9d56208fb9329f00e97a24fb
figueroaignacio <ignaciofigueroadev@gmail.com>
2024-06-04 16:10:25 -0300

    fix: fixing a pagination issue

--- app/(lobby)/posts/page.tsx
+++ app/(lobby)/posts/page.tsx
@@ -1,14 +1,77 @@
-import AllPosts from "@/components/posts/all-posts";
+import { posts } from "#site/content";
+import { PostCard } from "@/components/posts/post-card";
+import { QueryPagination } from "@/components/query-pagination";
+import { Tag } from "@/components/tag";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
-// Metadata
-import { Metadata } from "next";
+import { getAllCategories } from "@/utils/getAllCategories";
+import { sortCategoriesByCount } from "@/utils/sortCategoriesByCount";
+import { sortPosts } from "@/utils/sortPosts";
 
-export const metadata: Metadata = {
-  title: "Posts",
-  description:
-    "Explore, read and learn in high-quality posts in Frontend Society.",
-};
+const POSTS_PER_PAGE = 4;
 
-export default function PostsPage() {
-  return <AllPosts />;
+interface BlogPageProps {
+  searchParams: {
+    page?: string;
+  };
+}
+
+export default async function BlogPage({ searchParams }: BlogPageProps) {
+  const currentPage = Number(searchParams?.page) || 1;
+  const sortedPosts = sortPosts(posts.filter((post) => post.published));
+  const totalPages = Math.ceil(sortedPosts.length / POSTS_PER_PAGE);
+
+  const displayPosts = sortedPosts.slice(
+    POSTS_PER_PAGE * (currentPage - 1),
+    POSTS_PER_PAGE * currentPage
+  );
+
+  const tags = getAllCategories(posts);
+  const sortedTags = sortCategoriesByCount(tags);
+
+  return (
+    <section>
+      <div className="flex flex-col gap-12 mt-24">
+        <div className="flex flex-col gap-3">
+          <h1 className="font-bold text-2xl lg:text-4xl">
+            The ramblings of Front Society.
+          </h1>
+          <div className="mt-10">
+            <Card>
+              <CardHeader>
+                <CardTitle>Tags</CardTitle>
+              </CardHeader>
+              <CardContent className="flex flex-wrap gap-2">
+                {sortedTags.map((tag) => (
+                  <Tag tag={tag} key={tag} count={tags[tag]} />
+                ))}
+              </CardContent>
+            </Card>
+          </div>
+        </div>
+        {displayPosts.length > 0 ? (
+          <ul className="grid gap-4 grid-cols-1 sm:grid-cols-2 ">
+            {displayPosts.map((post) => {
+              return (
+                <li key={post.slug}>
+                  <PostCard
+                    slug={post.slug}
+                    date={post.date}
+                    title={post.title}
+                    description={post.description ?? ""}
+                    categories={post.categories}
+                  />
+                </li>
+              );
+            })}
+          </ul>
+        ) : (
+          <section className="py-36 text-center">
+            <p className="text-xl">Nothing to see, try with another word</p>
+          </section>
+        )}
+        <QueryPagination totalPages={totalPages} />
+      </div>
+    </section>
+  );
 }
