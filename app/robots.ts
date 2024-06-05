6eccf5199a4b56420ed565f3adbfd2641579ff30
figueroaignacio <ignaciofigueroadev@gmail.com>
2024-06-05 18:00:20 -0300

    feat(app): adding robots.ts file

---
+++ app/robots.ts
@@ -0,0 +1,15 @@
+// Config
+import { siteConfig } from "@/config/site";
+
+import { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+      disallow: "/private/",
+    },
+    sitemap: `${siteConfig.url}/sitemap.xml`,
+  };
+}
