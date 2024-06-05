201d11bf88e5ef0a95d59c37ec69f4a876a89bed
figueroaignacio <ignaciofigueroadev@gmail.com>
2024-06-05 18:22:14 -0300

    fix(config): removing slash

--- config/site.ts
+++ config/site.ts
@@ -8,7 +8,7 @@ export const siteConfig = Object.freeze({
   description:
     "Welcome where tech meets curiosity. Quick reads on web development, fun facts, history, tips, and more.",
   ogImage: "https://frontendsociety.netlify.app/og-home.png",
-  url: "https://frontendsociety.netlify.app/",
+  url: "https://frontendsociety.netlify.app",
   keywords: [
     "Blog",
     "Developers",
@@ -19,6 +19,6 @@ export const siteConfig = Object.freeze({
   ],
   links: {
     github: "https://github.com/figueroaignacio",
-    personalSite: "https://ignaciofigueroa.vercel.app/",
+    personalSite: "https://ignaciofigueroa.vercel.app",
   },
 });
