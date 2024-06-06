f572b64e5bf36329488257eb02ab0596a2a60746
figueroaignacio <ignaciofigueroadev@gmail.com>
2024-06-06 00:41:31 -0300

    style(components): changing the badge padding

--- components/ui/badge.tsx
+++ components/ui/badge.tsx
@@ -4,7 +4,7 @@ import * as React from "react";
 import { cn } from "@/lib/utils";
 
 const badgeVariants = cva(
-  "inline-flex items-center rounded-full border px-4 py-1.5 text-[.625rem] focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2",
+  "inline-flex items-center rounded-full border px-3 py-1 text-[.625rem] focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2",
   {
     variants: {
       variant: {
