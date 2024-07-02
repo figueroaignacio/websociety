79cfd1ee0f4562a78f41207524e2c3abaf23c824
figueroaignacio <ignaciofigueroadev@gmail.com>
2024-07-02 16:23:57 -0300

    feat: improve responsive design

--- components/hero.tsx
+++ components/hero.tsx
@@ -17,9 +17,9 @@ export function Hero() {
   const { theme } = useTheme();
 
   return (
-    <section className="py-32">
-      <div className="flex items-center gap-3 ">
-        <div className="flex flex-col gap-3">
+    <section className="pt-32">
+      <div className="flex items-center gap-3">
+        <div className="flex flex-col gap-3 text-center md:text-left">
           <h1 className="font-bold text-6xl">
             Welcome to <span className="gradient-text">{home.title}</span>
           </h1>
@@ -31,11 +31,22 @@ export function Hero() {
             alt="Home hero code"
             width={0}
             height={0}
+            className="hidden md:block"
           />
         </div>
       </div>
-      <div className="w-full flex justify-center py-10">
-        <ArrowDown className="border-zinc-950 border-1 animate-bounce" />
+      <div className="w-full flex justify-center py-10 flex-col items-center gap-2 text-muted-foreground">
+        <span>Scroll down</span>
+        <ArrowDown className="border-[1px] border-border w-8 h-8 rounded-full p-1 animate-bounce" />
+      </div>
+      <div>
+        <Image
+          src={theme === "dark" ? HeroCodeImageDark : HeroCodeImageLight}
+          alt="Home hero code"
+          width={0}
+          height={0}
+          className="block md:hidden"
+        />
       </div>
     </section>
   );
