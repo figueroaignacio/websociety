69cf8bf36cc7c2875151dce42b558b59e848bc1f
figueroaignacio <ignaciofigueroadev@gmail.com>
2024-07-01 23:00:12 -0300

    feat: improving hero section

--- components/hero.tsx
+++ components/hero.tsx
@@ -6,7 +6,7 @@ import { home } from "@/constants/home";
 
 export function Hero() {
   return (
-    <section className="flex flex-col gap-3 justify-center items-center py-40 text-center">
+    <section className="flex flex-col gap-3 justify-center items-center py-32 lg:py-52 text-center">
       <Badge
         variant="outline"
         className="px-4 text-sm bg-violet-600 text-white"
