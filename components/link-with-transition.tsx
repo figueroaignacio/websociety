182c1b1ad21c2170edf16205e012857716bfc7d9
figueroaignacio <ignaciofigueroadev@gmail.com>
2024-07-10 21:06:57 -0300

    feat: adding view transition api

--- components/link-with-transition.tsx
+++ components/link-with-transition.tsx
@@ -1,49 +1,24 @@
 "use client";
-import Link, { LinkProps } from "next/link";
+
+import Link from "next/link";
 import { useRouter } from "next/navigation";
-import React from "react";
 
-interface LinkWithTransitionProps extends LinkProps {
-  children: React.ReactNode;
-  href: string;
-  className?: string;
-}
-
-function sleep(ms: number): Promise<void> {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
-
-export const LinkWithTransition: React.FC<LinkWithTransitionProps> = ({
-  children,
-  href,
-  className,
-  ...props
-}) => {
+export function LinkWithTransition(props: any) {
   const router = useRouter();
-
-  const handleTransition = async (
-    e: React.MouseEvent<HTMLAnchorElement, MouseEvent>
-  ) => {
-    e.preventDefault();
-    const body = document.querySelector("body");
-
-    body?.classList.add("page-transition");
-
-    await sleep(200);
-    router.push(href);
-    await sleep(200);
-
-    body?.classList.remove("page-transition");
+  const handleClick = (e: React.MouseEvent<HTMLAnchorElement>) => {
+    if (!document.startViewTransition) {
+      return;
+    } else {
+      e.preventDefault();
+      document.startViewTransition(() => {
+        router.push(props.href);
+      });
+    }
   };
 
   return (
-    <Link
-      {...props}
-      href={href}
-      onClick={handleTransition}
-      className={className}
-    >
-      {children}
+    <Link onClick={handleClick} {...props}>
+      {props.children}
     </Link>
   );
-};
+}
