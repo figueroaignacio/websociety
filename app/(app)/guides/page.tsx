5fad74ba631fa8ee6968e60f321bec83ee51cee2
figueroaignacio <ignaciofigueroadev@gmail.com>
2024-07-11 17:11:45 -0300

    chore(app): change responsive design

--- app/(app)/guides/page.tsx
+++ app/(app)/guides/page.tsx
@@ -20,7 +20,7 @@ export default function LearnPage() {
     <section className="flex flex-col gap-12 mt-24">
       <h1 className="font-bold text-2xl lg:text-4xl">{g.title}</h1>
       {guides.length > 0 ? (
-        <ul className="grid gap-4 grid-cols-1 ">
+        <ul className="grid gap-4 grid-cols-1 md:grid-cols-2">
           {guides.map((path) => {
             return (
               <li key={path.slug}>
