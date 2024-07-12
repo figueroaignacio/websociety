1050601644fc17d6013187363ee75c0f1f95b43e
figueroaignacio <ignaciofigueroadev@gmail.com>
2024-07-12 00:48:42 -0300

    i dont know what i did

--- components/link-with-transition.tsx
+++ components/link-with-transition.tsx
@@ -1,9 +1,20 @@
 "use client";
 
+// Next
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 
-export function LinkWithTransition(props: any) {
+interface LinkWithTransitionProps {
+  children: React.ReactNode;
+  className: string;
+  href: string;
+}
+
+export function LinkWithTransition({
+  children,
+  className,
+  href,
+}: LinkWithTransitionProps) {
   const router = useRouter();
   const handleClick = (e: React.MouseEvent<HTMLAnchorElement>) => {
     if (!document.startViewTransition) {
@@ -11,14 +22,14 @@ export function LinkWithTransition(props: any) {
     } else {
       e.preventDefault();
       document.startViewTransition(() => {
-        router.push(props.href);
+        router.push(href);
       });
     }
   };
 
   return (
-    <Link onClick={handleClick} {...props}>
-      {props.children}
+    <Link onClick={handleClick} href={href} className={className}>
+      {children}
     </Link>
   );
 }
