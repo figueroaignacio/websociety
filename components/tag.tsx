15a30f9f31e90201a8eb803578d2516d549c8f3f
figueroaignacio <ignaciofigueroadev@gmail.com>
2024-07-13 00:51:53 -0300

    chore: remove link with transition from tag component

--- components/tag.tsx
+++ components/tag.tsx
@@ -1,5 +1,5 @@
 // Components
-import { LinkWithTransition } from "./link-with-transition";
+import Link from "next/link";
 import { badgeVariants } from "./ui/badge";
 
 // Utils
@@ -13,7 +13,7 @@ interface TagProps {
 
 export function Tag({ tag, count, current }: TagProps) {
   return (
-    <LinkWithTransition
+    <Link
       className={badgeVariants({
         variant: current ? "default" : "chip",
         className: "no-underline rounded-md duration-100",
@@ -21,6 +21,6 @@ export function Tag({ tag, count, current }: TagProps) {
       href={`/categories/${slug(tag)}`}
     >
       {tag} {count ? `(${count})` : null}
-    </LinkWithTransition>
+    </Link>
   );
 }
