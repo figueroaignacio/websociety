20976851ab80fdfb5fbdccfb784c3545c0249a07
figueroaignacio <ignaciofigueroadev@gmail.com>
2024-07-27 21:21:45 -0300

    style: rempove border dashed

--- components/hero.tsx
+++ components/hero.tsx
@@ -53,7 +53,7 @@ export function Hero() {
                 alt="Home hero code"
                 width={0}
                 height={0}
-                className="border-[1px] rounded-md bg-card border-dashed"
+                className="border-[1px] rounded-md bg-card"
               />
             </div>
           </FadeUp>
