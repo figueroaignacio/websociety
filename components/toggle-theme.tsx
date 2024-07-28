b395142a9a2a93a871c56a947facff5676e0cd98
figueroaignacio <ignaciofigueroadev@gmail.com>
2024-07-27 22:45:32 -0300

    style: improve toggle theme button component styles

--- components/toggle-theme.tsx
+++ components/toggle-theme.tsx
@@ -15,6 +15,7 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { Separator } from "./ui/separator";
 
 export function ToggleTheme() {
   const { setTheme } = useTheme();
@@ -22,13 +23,13 @@ export function ToggleTheme() {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="navItem" size="icon">
+        <Button variant="purple" size="icon">
           <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
           <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent align="end" className="flex flex-col gap-1 ">
+      <DropdownMenuContent align="end" className="flex flex-col gap-1">
         <DropdownMenuItem
           onClick={() => setTheme("light")}
           className="flex justify-between"
@@ -43,6 +44,7 @@ export function ToggleTheme() {
           <Moon />
           Dark
         </DropdownMenuItem>
+        <Separator />
         <DropdownMenuItem
           onClick={() => setTheme("system")}
           className="flex justify-between"
