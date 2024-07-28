0f38d361cda711d1f5feaf7926a61b809d201c71
figueroaignacio <ignaciofigueroadev@gmail.com>
2024-07-27 23:16:37 -0300

    style: improve responsive for tablets

--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -16,7 +16,7 @@ export function Navbar() {
   const pathname = usePathname();
 
   return (
-    <header className="flex items-center justify-between sticky z-20 top-0 left-0 py-2 px-3 lg:px-16 backdrop-blur-sm mx-auto border-b-[.0625rem]">
+    <header className="flex items-center justify-between sticky z-20 top-0 left-0 py-2 px-3 md:px-10 lg:px-16 backdrop-blur-sm mx-auto border-b-[.0625rem]">
       <div className="flex gap-9 items-center justify-between lg:justify-normal w-screen">
         <div className="flex items-center w-full md:hidden relative inset-0">
           <MobileMenu />
