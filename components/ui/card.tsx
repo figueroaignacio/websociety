e7b8da98b09268c066ae65610de676adb8afc6d6
figueroaignacio <ignaciofigueroadev@gmail.com>
2024-07-27 22:37:59 -0300

    style: change some card and gradient styles

--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -8,10 +8,7 @@ const Card = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <div
     ref={ref}
-    className={cn(
-      "rounded-lg border bg-transparent text-card-foreground",
-      className
-    )}
+    className={cn("rounded-lg border text-card-foreground bg-card", className)}
     {...props}
   />
 ));
