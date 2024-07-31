b3987e2f9eb5396ee8ae06b5926d89f6607dcb35
figueroaignacio <ignaciofigueroadev@gmail.com>
2024-07-31 15:40:50 -0300

    feat: add posts page skeleton loader

--- app/(lobby)/posts/loading.tsx
+++ app/(lobby)/posts/loading.tsx
@@ -1,6 +1,69 @@
 // Components
-import { Loader } from "@/components/layout/loader";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Skeleton } from "@/components/ui/skeleton";
 
 export default function Loading() {
-  return <Loader />;
+  return (
+    <section>
+      <div className="flex flex-col gap-12 mt-24 max-w-5xl mx-auto">
+        <div className="flex flex-col gap-3">
+          <Skeleton className="w-52 lg:w-96 h-4" />
+          <div className="mt-10">
+            <Card>
+              <CardHeader>
+                <CardTitle>
+                  <Skeleton className="w-1/3 h-4" />
+                </CardTitle>
+              </CardHeader>
+              <CardContent className="flex flex-wrap gap-2">
+                {Array.from({ length: 21 }).map((_, index) => (
+                  <Skeleton
+                    className={`h-6 ${index % 3 === 1 ? "w-20" : "w-12"}`}
+                    key={index}
+                  />
+                ))}
+              </CardContent>
+            </Card>
+          </div>
+        </div>
+        <div>
+          <ul className="grid gap-4 grid-cols-1 sm:grid-cols-2">
+            {Array.from({ length: 4 }).map((_, index) => (
+              <li key={index}>
+                <Card>
+                  <CardHeader>
+                    <CardTitle>
+                      <Skeleton className="w-48 h-4" />
+                    </CardTitle>
+                  </CardHeader>
+                  <CardContent>
+                    <CardDescription className="flex flex-col gap-2">
+                      {Array.from({ length: 3 }).map((_, index) => (
+                        <Skeleton className="w-72 h-4" key={index} />
+                      ))}
+                    </CardDescription>
+                  </CardContent>
+                  <CardFooter className="flex-col items-start gap-4">
+                    <div className="flex gap-3 items-center">
+                      {Array.from({ length: 3 }).map((_, index) => (
+                        <Skeleton className="w-16 h-4" key={index} />
+                      ))}
+                    </div>
+                    <Skeleton className="w-full h-8" />
+                  </CardFooter>
+                </Card>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </section>
+  );
 }
--- components/ui/skeleton.tsx
+++ components/ui/skeleton.tsx
@@ -6,7 +6,10 @@ function Skeleton({
 }: React.HTMLAttributes<HTMLDivElement>) {
   return (
     <div
-      className={cn("animate-pulse rounded-md bg-muted", className)}
+      className={cn(
+        "animate-pulse rounded-md bg-gray-300 dark:bg-gray-700/50",
+        className
+      )}
       {...props}
     />
   );
