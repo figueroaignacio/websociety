8ec194c63941de75c6c89d11385e01748a97a845
figueroaignacio <ignaciofigueroadev@gmail.com>
2024-08-01 21:12:59 -0300

    feat: change opengraph image

--- src/app/opengraph-image.tsx
+++ src/app/opengraph-image.tsx
@@ -15,23 +15,25 @@ export const contentType = "image/png";
 export default async function Image() {
   return new ImageResponse(
     (
-      <div
-        style={{
-          fontWeight: "bold",
-          background: "#09090b",
-          color: "white",
-          width: "100%",
-          height: "100%",
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-      >
-        <div>
+      <section style={{ width: 1200, height: 630 }}>
+        <div
+          style={{
+            fontWeight: "bold",
+            background: "#09090b",
+            color: "white",
+            width: "100%",
+            height: "100%",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+            gap: 5,
+            position: "relative",
+          }}
+        >
           <svg
-            width="34"
-            height="34"
+            width="124"
+            height="124"
             viewBox="0 0 34 34"
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
@@ -45,9 +47,12 @@ export default async function Image() {
             <rect x="13" y="14" width="10" height="5" rx="2.5" fill="#F5F5F5" />
             <circle cx="27.5" cy="24.5" r="1.5" fill="#F5F5F5" />
           </svg>
-          <div>Frontend Society</div>
+          <div style={{ textAlign: "center" }}>Frontend Society</div>
+          <div style={{ textAlign: "center" }}>
+            By a developer, for developers.
+          </div>
         </div>
-      </div>
+      </section>
     ),
     {
       ...size,
