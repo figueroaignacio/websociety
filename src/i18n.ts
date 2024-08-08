3722038f999c8b40e3cb647920369ac1de484188
figueroaignacio <ignaciofigueroadev@gmail.com>
2024-08-08 18:29:32 -0300

    chore: change from messages to locales folder

--- src/i18n.ts
+++ src/i18n.ts
@@ -6,6 +6,6 @@ export default getRequestConfig(async () => {
 
   return {
     locale,
-    messages: (await import(`./messages/${locale}.json`)).default,
+    messages: (await import(`./locales/${locale}.json`)).default,
   };
 });
