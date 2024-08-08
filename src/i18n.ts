79d3246fda2ee517954faa5b7c830680f1ed4834
figueroaignacio <ignaciofigueroadev@gmail.com>
2024-08-08 18:45:36 -0300

    feat: add es and en folders

--- src/i18n.ts
+++ src/i18n.ts
@@ -4,8 +4,19 @@ import { getUserLocale } from "./services/locale";
 export default getRequestConfig(async () => {
   const locale = await getUserLocale();
 
+  const modules = ["hero"];
+
+  const messages = {};
+
+  for (const moduleName of modules) {
+    const moduleMessages = await import(
+      `./locales/${locale}/${moduleName}.json`
+    );
+    Object.assign(messages, moduleMessages.default);
+  }
+
   return {
     locale,
-    messages: (await import(`./locales/${locale}.json`)).default,
+    messages,
   };
 });
