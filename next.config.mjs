ac5bb231618200cf73b8af2e48859286fd420ade
figueroaignacio <ignaciofigueroadev@gmail.com>
2024-08-16 17:45:04 -0300

    chore: next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,11 @@
+// next-intl
 import createNextIntlPlugin from "next-intl/plugin";
 const withNextIntl = createNextIntlPlugin();
 
+// Velite
 const isDev = process.argv.indexOf("dev") !== -1;
 const isBuild = process.argv.indexOf("build") !== -1;
+
 if (!process.env.VELITE_STARTED && (isDev || isBuild)) {
   process.env.VELITE_STARTED = "1";
   const { build } = await import("velite");
@@ -12,21 +15,6 @@ if (!process.env.VELITE_STARTED && (isDev || isBuild)) {
 /**
  * @type {import('next').NextConfig}
  */
-const nextConfig = {
-  trailingSlash: false,
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          {
-            key: "Permissions-Policy",
-            value: "join-ad-interest-group=()",
-          },
-        ],
-      },
-    ];
-  },
-};
+const nextConfig = {};
 
 export default withNextIntl(nextConfig);
