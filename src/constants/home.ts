15237e96de955706573343ba06193dddfa6e2341
figueroaignacio <ignaciofigueroadev@gmail.com>
2024-08-19 16:52:22 -0300

    feat: remove unnecesary constants files

--- src/constants/home.ts
+++
@@ -1,6 +0,0 @@
-export const home = Object.freeze({
-  title: "Frontend Society",
-  description:
-    "And discover tech posts, learn about Frontend stuff, and participate on challenges.",
-  badge: "By a developer, for developers 🚀",
-});
