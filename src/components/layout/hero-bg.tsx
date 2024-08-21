efaf1ac53e98e6dad7fff0302c4c799514dba093
figueroaignacio <ignaciofigueroadev@gmail.com>
2024-08-21 16:59:30 -0300

    style: change text gradient

--- src/components/layout/hero-bg.tsx
+++ src/components/layout/hero-bg.tsx
@@ -2,7 +2,7 @@ export function HeroBg() {
   return (
     <div className="w-full h-full -z-10 absolute flex flex-row justify-between left-0 lg:h-full lg:max-w-7xl lg:px-0 mx-auto px-6 right-0">
       <div className="w-full h-full border-border border-x border-dashed"></div>
-      <div className="w-full h-full border-border border-x border-dashed"></div>
+      {/* <div className="w-full h-full border-border border-x border-dashed"></div> */}
       <div className="w-full h-full border-border border-x border-dashed hidden md:block"></div>
     </div>
   );
