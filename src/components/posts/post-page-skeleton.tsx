7026a6306b1631f199616802ea7579de4931db50
figueroaignacio <ignaciofigueroadev@gmail.com>
2024-09-22 14:53:19 -0300

    style: improve post page skeleton

--- src/components/posts/post-page-skeleton.tsx
+++ src/components/posts/post-page-skeleton.tsx
@@ -1,15 +1,42 @@
 // Components
+import { Separator } from "../ui/separator";
 import { Skeleton } from "../ui/skeleton";
 
 export function PostPageSkeleton() {
   return (
     <article className="mx-auto grid grid-cols-1 lg:grid-cols-12 gap-8 relative top-12">
       <aside className="hidden lg:block lg:col-span-3">
-        <div className="p-4 space-y-3">
-          <Skeleton className="w-full h-10" />
-          <Skeleton className="w-full h-10" />
-          <Skeleton className="w-full h-10" />
-          <Skeleton className="w-full h-10" />
+        <div className="flex items-center gap-2">
+          <Skeleton className="size-12 rounded-full" />
+          <Skeleton className="w-28 h-5" />
+        </div>
+        <Separator className="my-8" />
+        <div className="space-y-3">
+          <div className="flex items-center gap-2">
+            <Skeleton className="size-6" />
+            <Skeleton className="w-28 h-2" />
+          </div>
+          <div className="flex items-center gap-2 flex-wrap">
+            <Skeleton className="size-6" />
+            {Array.from({ length: 8 }).map((_, index) => (
+              <Skeleton className="w-10 h-4" key={index} />
+            ))}
+          </div>
+        </div>
+        <div className="my-8 space-y-4">
+          <Skeleton className="w-36 h-6" />
+          <div className="space-y-2">
+            <Skeleton className="w-full h-4" />
+            <Skeleton className="w-56 h-4" />
+            <Skeleton className="w-28 h-4" />
+          </div>
+          <div className="flex flex-wrap items-center gap-3">
+            <Skeleton className="w-28 h-7" />
+            <Skeleton className="w-28 h-7" />
+            <Skeleton className="w-32 h-7" />
+            <Skeleton className="w-28 h-7" />
+            <Skeleton className="w-40 h-7" />
+          </div>
         </div>
       </aside>
       <div className="lg:col-span-6 top-4 relative">
