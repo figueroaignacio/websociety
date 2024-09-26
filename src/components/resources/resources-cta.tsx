803f991dc789ed2cbaa7033be440c099b3b11639
figueroaignacio <ignaciofigueroadev@gmail.com>
2024-09-26 16:19:25 -0300

    feat: add resources cta translations

--- src/components/resources/resources-cta.tsx
+++ src/components/resources/resources-cta.tsx
@@ -1,3 +1,7 @@
+// Hooks
+import { useTranslations } from "next-intl";
+
+// Components
 import { Button } from "@/components/ui/button";
 import {
   Card,
@@ -6,27 +10,28 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+
+// Icons
 import { ArrowRight, Code2, Library } from "lucide-react";
 
 export function ResourcesCta() {
+  const t = useTranslations("resourcesCta");
+
   return (
     <Card className="shadow-custom-card">
       <CardHeader>
         <CardTitle className="text-2xl font-bold flex items-center gap-2">
           <Code2 className="w-6 h-6" />
-          Web Dev Resources
+          {t("title")}
         </CardTitle>
       </CardHeader>
       <CardContent>
-        <p className="text-muted-foreground">
-          Discover a treasure trove of curated resources for web developers.
-          Boost your skills and stay up-to-date with the latest tools and
-          technologies.
-        </p>
+        <p className="text-muted-foreground">{t("description")}</p>
       </CardContent>
       <CardFooter className="flex justify-end w-full">
         <Button className="w-full ">
-          <Library className="mr-2 h-4 w-4" /> Explora los recursos
+          <Library className="mr-2 h-4 w-4" />
+          <span>{t("button")}</span>
           <ArrowRight className="ml-2 h-4 w-4" />
         </Button>
       </CardFooter>
