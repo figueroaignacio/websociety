4f2c237b5c03d0fd7ad8451535ba3477cd2a195f
figueroaignacio <ignaciofigueroadev@gmail.com>
2024-09-27 11:45:24 -0300

    feat: improve footer section

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,52 +1,56 @@
-// Hooks
-import { useTranslations } from "next-intl";
-
-// Components
 import { Link } from "@/config/navigation";
-
-// Icons
 import { Copyright } from "lucide-react";
+import { useTranslations } from "next-intl";
 
 export function Footer() {
   const t = useTranslations();
   const navigation = t.raw("navigation");
 
   return (
-    <footer className="w-full border-t-[.0625rem] mt-24 mb-16">
-      <div className=" px-4 lg:px-0 max-w-5xl mx-auto">
-        <div className="grid grid-cols-1 sm:grid-cols-2 gap-8 md:gap-12 justify-center py-12">
-          <div>
-            <h4 className="text-lg font-semibold">{t("footer.title")}</h4>
-            <p>{t("footer.subtitle")}</p>
-            <nav>
-              <ul className="flex flex-col gap-2 py-2">
-                {navigation.map((navItem: any, index: number) => (
-                  <li key={index} className="flex items-center">
-                    <Link
-                      href={navItem.href}
-                      className="text-base text-muted-foreground hover:text-black dark:hover:text-white font-medium"
-                    >
-                      {navItem.title}
-                    </Link>
-                  </li>
-                ))}
-              </ul>
-            </nav>
+    <footer className="border-t mt-24 mb-16">
+      <div className="container mx-auto px-4 py-12 md:py-16">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mx-auto ">
+          <div className="space-y-4">
+            <h2 className="text-lg font-semibold">{t("footer.title")}</h2>
+            <p className="text-muted-foreground">{t("footer.subtitle")}</p>
           </div>
-          <div>
-            <h4 className="text-lg font-semibold mb-4">
+          <nav aria-label="Footer Navigation">
+            <h2 className="text-lg font-semibold mb-4">
+              {t("footer.navigation.title")}
+            </h2>
+            <ul className="space-y-2">
+              {navigation.map((navItem: any, index: number) => (
+                <li key={index}>
+                  <Link
+                    href={navItem.href}
+                    className="text-muted-foreground hover:text-foreground transition-colors"
+                  >
+                    {navItem.title}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </nav>
+          <div className="space-y-4">
+            <h2 className="text-lg font-semibold">
               {t("footer.aboutHeading")}
-            </h4>
+            </h2>
             <p className="text-muted-foreground">{t("footer.description")}</p>
           </div>
+          {/* <div className="space-y-4">
+            <h2 className="text-lg font-semibold">{t("footer.contact")}</h2>
+            <address className="not-italic text-muted-foreground">
+              <p>{t("footer.address")}</p>
+              <p>{t("footer.email")}</p>
+              <p>{t("footer.phone")}</p>
+            </address>
+          </div> */}
         </div>
-        <div className="mt-8 md:mt-12 text-center text-sm">
-          <div className="flex items-center flex-col justify-center gap-2">
-            <Copyright className="h-4 w-4 text-muted-foreground" />
-            <span className="text-muted-foreground">
-              {t("footer.copyright")}
-            </span>
-          </div>
+        <div className="mt-12 pt-8 text-center">
+          <p className="text-sm text-muted-foreground flex items-center justify-center gap-2">
+            <Copyright className="h-4 w-4" />
+            <span>{t("footer.copyright")}</span>
+          </p>
         </div>
       </div>
     </footer>
