ec863029a6c33cc41b3157f5d4dbd11711e2072f
figueroaignacio <ignaciofigueroadev@gmail.com>
2024-09-27 11:27:15 -0300

    feat: improve landing

--- src/components/resources/resource-item.tsx
+++ src/components/resources/resource-item.tsx
@@ -19,15 +19,15 @@ interface ResourceItemProps {
 
 export function ResourceItem({ icon, resource }: ResourceItemProps) {
   return (
-    <Card className="rounded-lg text-center">
+    <Card className="rounded-lg text-center gradient-background text-white">
       <CardHeader className="justify-center items-center">
-        <div className="mb-4 border-2 p-4 rounded-full border-dashed">
+        <div className="w-16 h-16 rounded-full gradient-background flex items-center justify-center">
           {icon}
         </div>
         <h3 className="text-lg font-semibold">{resource.title}</h3>
       </CardHeader>
       <CardContent>
-        <CardDescription className="text-gray-600">
+        <CardDescription className="text-white">
           {resource.description}
         </CardDescription>
       </CardContent>
--- src/sections/features.tsx
+++ src/sections/features.tsx
@@ -1,24 +1,12 @@
-// Hooks
+import { FeatureItem } from "@/components/feature-item";
+import { BookCheck, Code2, Globe, GraduationCap } from "lucide-react";
 import { useTranslations } from "next-intl";
 
-// Components
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-
-// Icons
-import { BookCheck, Code2, Globe, GraduationCap } from "lucide-react";
-
-// Icons mapping object
 const iconMap: { [key: string]: JSX.Element } = {
-  book: <BookCheck size="3rem" />,
-  learn: <GraduationCap size="3rem" />,
-  code: <Code2 size="3rem" />,
-  resources: <Globe size="3rem" />,
+  book: <BookCheck size="2rem" />,
+  learn: <GraduationCap size="2rem" />,
+  code: <Code2 size="2rem" />,
+  resources: <Globe size="2rem" />,
 };
 
 export function Features() {
@@ -26,32 +14,13 @@ export function Features() {
   const features = t.raw("features");
 
   return (
-    <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 gap-3">
+    <section className="grid grid-cols-1 sm:grid-cols-2 gap-6 rounded-3xl">
       {features.map(
         (
           feature: { title: string; description: string; icon: string },
           index: number
         ) => {
-          const icon = iconMap[feature.icon] || null;
-          return (
-            <Card className="h-full w-full rounded-2xl" key={index}>
-              <CardHeader className="py-12 gradient-background rounded-t-2xl">
-                <CardTitle className="flex flex-col justify-center items-center text-center gap-2 text-xs md:text-lg">
-                  {icon}
-                  {feature.title}
-                </CardTitle>
-              </CardHeader>
-              <CardContent className="py-12 flex h-full">
-                <CardDescription
-                  className={`h-full w-full text-center text-base max-w-xl mx-auto ${
-                    index === 2 ? "sm:col-span-2" : ""
-                  }`}
-                >
-                  {feature.description}
-                </CardDescription>
-              </CardContent>
-            </Card>
-          );
+          return <FeatureItem key={index} feature={feature} index={index} />;
         }
       )}
     </section>
