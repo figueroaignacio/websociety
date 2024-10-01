d659e3c9bd8f5d4f964ee0e8974c44b42db5bdc9
figueroaignacio <ignaciofigueroadev@gmail.com>
2024-10-01 18:09:40 -0300

    fix: fix imports

--- app/[locale]/(lobby)/guides/[...slug]/loading.tsx
+++ app/[locale]/(lobby)/guides/[...slug]/loading.tsx
@@ -1,5 +1,5 @@
 // Components
-import { Loader } from "../../../../../../components/loader";
+import { Loader } from "@/components/loader";
 
 export default function Loading() {
   return <Loader />;
