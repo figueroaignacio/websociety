eda63dfe73eca143935012b1d3104c506685c0a6
figueroaignacio <ignaciofigueroadev@gmail.com>
2024-10-28 16:09:09 -0300

    feat: change support links place

--- app/[locale]/(lobby)/settings/page.tsx
+++ app/[locale]/(lobby)/settings/page.tsx
@@ -4,18 +4,11 @@
 import { useTranslations } from "next-intl";
 
 // Components
-import { Separator } from "@/components/ui/separator";
 import { LocaleSwitcher } from "@/modules/settings/ui/locale-switcher";
-import { SupportLink } from "@/modules/settings/ui/support-link";
 import { ToggleTheme } from "@/modules/settings/ui/toggle-theme";
-import {
-  Bug,
-  GithubIcon,
-  HelpCircle,
-  Mail,
-  SettingsIcon,
-  Sliders,
-} from "lucide-react";
+
+// Icons
+import { SettingsIcon, Sliders } from "lucide-react";
 
 const SectionHeader: React.FC<{ icon: React.ReactNode; title: string }> = ({
   icon,
@@ -30,31 +23,12 @@ const SectionHeader: React.FC<{ icon: React.ReactNode; title: string }> = ({
 export default function SettingsPage() {
   const t = useTranslations();
 
-  const supportLinks = [
-    {
-      href: "https://github.com/figueroaignacio/frontendsociety/issues",
-      title: t("support.reportBug.title"),
-      icon: <Bug size={16} />,
-    },
-    {
-      href: "mailto:ignaciofigueroadev@gmail.com",
-      title: t("support.developerContact.title"),
-      icon: <Mail size={16} />,
-    },
-    {
-      href: "https://github.com/figueroaignacio/frontendsociety",
-      title: t("support.sourceCode.title"),
-      icon: <GithubIcon size={16} />,
-    },
-  ];
-
   return (
-    <div className="w-full mx-auto px-4 py-8">
+    <div className="w-full mx-auto px-4 py-8 max-w-3xl">
       <div className="flex items-center gap-2 mb-8">
         <SettingsIcon size={28} className="text-primary" />
         <h1 className="text-3xl font-bold">{t("settings.title")}</h1>
       </div>
-
       <div className="space-y-8">
         <section>
           <SectionHeader
@@ -64,20 +38,6 @@ export default function SettingsPage() {
           <div className="space-y-6">
             <ToggleTheme />
             <LocaleSwitcher />
-          </div>
-        </section>
-
-        <Separator />
-
-        <section>
-          <SectionHeader
-            icon={<HelpCircle size={20} className="text-primary" />}
-            title={t("support.title")}
-          />
-          <div className="grid gap-4">
-            {supportLinks.map((link, index) => (
-              <SupportLink key={index} {...link} />
-            ))}
           </div>
         </section>
       </div>
--- modules/navigation/ui/app-sidebar.tsx
+++ modules/navigation/ui/app-sidebar.tsx
@@ -21,7 +21,15 @@ import {
 import { Link } from "@/config/i18n/routing";
 
 // Icons
-import { BookOpen, FileText, Folder, Settings } from "lucide-react";
+import {
+  BookOpen,
+  Bug,
+  FileText,
+  Folder,
+  GithubIcon,
+  Mail,
+  Settings,
+} from "lucide-react";
 
 // Utils
 import { cn } from "@/lib/utils";
@@ -37,12 +45,33 @@ const iconMap = {
   bookOpen: BookOpen,
   folder: Folder,
   settings: Settings,
+  bug: Bug,
+  mail: Mail,
+  github: GithubIcon,
 };
 
 export function AppSidebar() {
   const t = useTranslations();
   const navigation: NavigationItem[] = t.raw("sidebarNavigation");
   const pathname = usePathname();
+
+  const supportLinks: NavigationItem[] = [
+    {
+      href: "https://github.com/figueroaignacio/websociety/issues/new",
+      title: t("support.reportBug.title"),
+      icon: "bug",
+    },
+    {
+      href: "mailto:ignaciofigueroadev@gmail.com",
+      title: t("support.developerContact.title"),
+      icon: "mail",
+    },
+    {
+      href: "https://github.com/figueroaignacio/frontendsociety",
+      title: t("support.sourceCode.title"),
+      icon: "github",
+    },
+  ];
 
   return (
     <Sidebar collapsible="icon">
@@ -57,7 +86,7 @@ export function AppSidebar() {
           <Searcher />
         </SidebarGroup>
         <SidebarGroup>
-          <SidebarGroupLabel>Application</SidebarGroupLabel>
+          <SidebarGroupLabel>Inicio</SidebarGroupLabel>
           <SidebarGroupContent>
             <SidebarMenu>
               {navigation.map((item) => {
@@ -81,6 +110,39 @@ export function AppSidebar() {
                         {IconComponent && <IconComponent />}
                         <span>{item.title}</span>
                       </Link>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                );
+              })}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+        <SidebarGroup>
+          <SidebarGroupLabel>Soporte</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {supportLinks.map((item) => {
+                const IconComponent = iconMap[item.icon];
+                const isActive = pathname === item.href;
+                return (
+                  <SidebarMenuItem key={item.title}>
+                    <SidebarMenuButton
+                      asChild
+                      className={cn(
+                        "rounded-sm transition-colors dark:hover:bg-gray-600 dark:hover:bg-opacity-30 hover:bg-gray-200 hover:bg-opacity-50 duration-100 text-muted-foreground hover:text-foreground text-sm",
+                        isActive &&
+                          "dark:bg-gray-600 dark:bg-opacity-30 bg-gray-200 bg-opacity-50 text-foreground"
+                      )}
+                      tooltip={item.title}
+                    >
+                      <a
+                        href={item.href}
+                        target="_blank"
+                        className="flex items-center py-2 px-4 gap-2"
+                      >
+                        {IconComponent && <IconComponent size={16} />}
+                        <span>{item.title}</span>
+                      </a>
                     </SidebarMenuButton>
                   </SidebarMenuItem>
                 );
