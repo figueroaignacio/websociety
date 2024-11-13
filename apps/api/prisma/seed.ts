bab68af05e6e72f3aa4d8d33ca10a71a90d30684
figueroaignacio <ignaciofigueroadev@gmail.com>
2024-11-13 15:04:02 -0300

    feat: add seed and swagger

---
+++ apps/api/prisma/seed.ts
@@ -0,0 +1,40 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const post1 = await prisma.post.upsert({
+    where: { id: 1 },
+    update: {},
+    create: {
+      title: 'Prisma Adds Support for MongoDB',
+      body: 'Support for MongoDB has been one of the most requested features since the initial release of...',
+      description:
+        "We are excited to share that today's Prisma ORM release adds stable support for MongoDB!",
+      published: false,
+    },
+  });
+
+  const post2 = await prisma.post.upsert({
+    where: { id: 2 },
+    update: {},
+    create: {
+      title: "What's new in Prisma? (Q1/22)",
+      body: 'Our engineers have been working hard, issuing new releases with many improvements...',
+      description:
+        'Learn about everything in the Prisma ecosystem and community from January to March 2022.',
+      published: true,
+    },
+  });
+
+  console.log({ post1, post2 });
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('/api');
+
+  const config = new DocumentBuilder()
+    .setTitle('Project Web Society')
+    .setDescription('The Web Society API')
+    .setVersion('0.1')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
