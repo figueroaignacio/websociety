a34b74103ef3c5488c75e8285e62010db897bf33
figueroaignacio <ignaciofigueroadev@gmail.com>
2024-12-24 02:11:47 -0300

    feat: add sign out dialog

--- apps/web/modules/auth/ui/signout-button.tsx
+++ apps/web/modules/auth/ui/signout-button.tsx
@@ -1,19 +1,61 @@
 "use client";
 
+import { useTransition } from "react";
+
+// Components
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+
 // Utils
 import { signOut } from "next-auth/react";
 
 export function SignOutButton() {
+  const [isPending, startTransition] = useTransition();
+
   const handleSignOut = async () => {
-    await signOut({
-      redirect: true,
-      callbackUrl: "/auth/login",
+    startTransition(async () => {
+      await signOut({
+        redirect: true,
+        callbackUrl: "/auth/login",
+      });
     });
   };
 
   return (
-    <button onClick={handleSignOut} className="btn">
-      Cerrar sesión
-    </button>
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button className="btn">Cerrar sesión</Button>
+      </DialogTrigger>
+
+      <DialogContent>
+        <DialogTitle>¿Estás seguro?</DialogTitle>
+        <DialogDescription>
+          ¿Estás seguro de que deseas cerrar sesión? Esta acción te
+          desconectará.
+        </DialogDescription>
+        <DialogFooter>
+          <DialogClose asChild>
+            <Button type="button" variant="secondary">
+              Close
+            </Button>
+          </DialogClose>
+          <Button
+            onClick={handleSignOut}
+            disabled={isPending}
+            variant="destructive"
+          >
+            {isPending ? "Cerrando sesion..." : "Confirmar"}
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
   );
 }
